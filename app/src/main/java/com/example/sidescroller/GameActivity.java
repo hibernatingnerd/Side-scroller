a0d547d9d95d56833be8def3554371c6e46bf327
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-10-17 10:58:17 -0400

    player class and enemy class created. Enemy draw-in functioning. next collision detection.

--- app/src/main/java/com/example/sidescroller/GameActivity.java
+++ app/src/main/java/com/example/sidescroller/GameActivity.java
@@ -1,25 +1,36 @@
 package com.example.sidescroller;
 
 
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 
 public class GameActivity extends AppCompatActivity {
 
     //declaring gameview
     private GameView gameView;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //Getting display object
+        Display display = getWindowManager().getDefaultDisplay();
+
+        //Getting the screen resolution into point object
+        Point size = new Point();
+        display.getSize(size);
+
         //Initializing game view object
-        gameView = new GameView(this);
+        //this time we are also passing the screen size to the GameView constructor
+        gameView = new GameView(this, size.x, size.y);
 
         //adding it to contentview
         setContentView(gameView);
     }
-
     //pausing the game when activity is paused
     @Override
     protected void onPause() {
--- app/src/main/java/com/example/sidescroller/GameView.java
+++ app/src/main/java/com/example/sidescroller/GameView.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -11,24 +13,32 @@ public class GameView extends SurfaceView implements Runnable {
 
     volatile boolean playing;
     private Thread gameThread = null;
-
-    //adding the player to this class
     private Player player;
 
-    //These objects will be used for drawing
     private Paint paint;
     private Canvas canvas;
     private SurfaceHolder surfaceHolder;
 
-    public GameView(Context context) {
+    //Adding enemies object array
+    private Enemy[] enemies;
+
+    //Adding 3 enemies you may increase the size
+    private int enemyCount = 3;
+
+
+    public GameView(Context context, int screenX, int screenY) {
         super(context);
+        player = new Player(context, screenX, screenY);
 
-        //initializing player object
-        player = new Player(context);
-
-        //initializing drawing objects
         surfaceHolder = getHolder();
         paint = new Paint();
+
+
+        //initializing enemy object array
+        enemies = new Enemy[enemyCount];
+        for(int i=0; i<enemyCount; i++){
+            enemies[i] = new Enemy(context, screenX, screenY);
+        }
     }
 
     @Override
@@ -41,25 +51,45 @@ public class GameView extends SurfaceView implements Runnable {
     }
 
     private void update() {
-        //updating player position
         player.update();
+
+
+        for(int i=0; i<enemyCount; i++){
+            enemies[i].update(player.getSpeed());
+
+            //if collision occurrs with player
+            if (Rect.intersects(player.getDetectCollision(), enemies[i].getDetectCollision())) {
+                //moving enemy outside the left edge
+                enemies[i].setX(-200);
+            }
+        }
     }
 
     private void draw() {
-        //checking if surface is valid
         if (surfaceHolder.getSurface().isValid()) {
-            //locking the canvas
             canvas = surfaceHolder.lockCanvas();
-            //drawing a background color for canvas
             canvas.drawColor(Color.BLACK);
-            //Drawing the player
+
+
+
             canvas.drawBitmap(
                     player.getBitmap(),
                     player.getX(),
                     player.getY(),
                     paint);
-            //Unlocking the canvas
+
+            //drawing the enemies
+            for (int i = 0; i < enemyCount; i++) {
+                canvas.drawBitmap(
+                        enemies[i].getBitmap(),
+                        enemies[i].getX(),
+                        enemies[i].getY(),
+                        paint
+                );
+            }
+
             surfaceHolder.unlockCanvasAndPost(canvas);
+
         }
     }
 
@@ -83,5 +113,19 @@ public class GameView extends SurfaceView implements Runnable {
         playing = true;
         gameThread = new Thread(this);
         gameThread.start();
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                player.stopBoosting();
+                break;
+            case MotionEvent.ACTION_DOWN:
+                player.setBoosting();
+                break;
+        }
+        return true;
     }
 }
